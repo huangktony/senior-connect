87e7f751c2f0f9bed3a0a38484249f56939c4104
aryanjverma <aryanjverma2@gmail.com>
2025-10-17 12:00:31 -0500

    creates card component, puts it on index to test

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,15 +1,8 @@
 import { Text, View } from "react-native";
+import Card from "../components/Card";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <Card title="Laptop" body="How do I turn on my laptop?" status="Pending"/>
   );
 }
