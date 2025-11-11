017d49a5f5334db74621e0561ea64635ecfd96ec
Aryan Verma <aryanjverma2@gmail.com>
2025-11-10 21:04:16 -0600

    merges login and elder

--- frontend/app/(tabs)/_layout.tsx
+++ frontend/app/(tabs)/_layout.tsx
@@ -1,35 +1,36 @@
 import { Tabs } from "expo-router";
 import { Ionicons } from "@expo/vector-icons";
+import { View } from "react-native";
 
 export default function TabsLayout() {
   
   return (
-    <Tabs screenOptions={{ headerShown: false }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: "Home",
-          tabBarLabelStyle: {fontSize: 10, },
-          tabBarIcon: ({ color, size }) => <Ionicons name="home" color={color} size={size} />,
-          
-        }}
-      />
-      <Tabs.Screen
-        name="Chat"
-        options={{
-          title: "Voice Assitant",
-          tabBarLabelStyle: {fontSize: 10, },
-          tabBarIcon: ({ color, size }) => <Ionicons name="sparkles" color={color} size={size} />,
-        }}
-      />
-      <Tabs.Screen
-        name="Profile"
-        options={{
-          title: "Profile",
-          tabBarLabelStyle: {fontSize: 10, },
-          tabBarIcon: ({ color, size }) => <Ionicons name="person" color={color} size={size} />,
-        }}
-      />
-    </Tabs>
+      <Tabs screenOptions={{ headerShown: false }}>
+        <Tabs.Screen
+          name="index"
+          options={{
+            title: "Home",
+            tabBarLabelStyle: {fontSize: 10, },
+            tabBarIcon: ({ color, size }) => <Ionicons name="home" color={color} size={size} />,
+            
+          }}
+        />
+        <Tabs.Screen
+          name="Chat"
+          options={{
+            title: "Voice Assitant",
+            tabBarLabelStyle: {fontSize: 10, },
+            tabBarIcon: ({ color, size }) => <Ionicons name="sparkles" color={color} size={size} />,
+          }}
+        />
+        <Tabs.Screen
+          name="Profile"
+          options={{
+            title: "Profile",
+            tabBarLabelStyle: {fontSize: 10, },
+            tabBarIcon: ({ color, size }) => <Ionicons name="person" color={color} size={size} />,
+          }}
+        />
+      </Tabs>
   );
 }
--- frontend/app/(tabs)/index.tsx
+++ frontend/app/(tabs)/index.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { Provider as PaperProvider } from "react-native-paper";
 import Board from "../../components/Board";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function App() {
   return (
-    <PaperProvider>
-      <Board />
-    </PaperProvider>
+    <SafeAreaView style={{ flex: 1, backgroundColor: "#f3f4f6" }}>
+      <PaperProvider>
+        <Board />
+      </PaperProvider>
+    </SafeAreaView>
   );
 }
